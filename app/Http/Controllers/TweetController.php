8565d65c8108502a4f2f1c97d0d17d990ac8a24f
ruirui23 <ruirui23@users.noreply.github.com>
2025-09-09 19:18:25 +0900

    add:Tweetの更新、削除処理の実装

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -43,7 +43,7 @@ class TweetController extends Controller
      */
     public function show(Tweet $tweet)
     {
-        //
+        return view('tweets.show', compact('tweet'));
     }
 
     /**
@@ -51,7 +51,7 @@ class TweetController extends Controller
      */
     public function edit(Tweet $tweet)
     {
-        //
+        return view('tweets.edit', compact('tweet'));
     }
 
     /**
@@ -59,14 +59,22 @@ class TweetController extends Controller
      */
     public function update(Request $request, Tweet $tweet)
     {
-        //
-    }
+      $request->validate([
+       'tweet' => 'required|max:255',
+    ]);
+
+    $tweet->update($request->only('tweet'));
+
+    return redirect()->route('tweets.show', $tweet);
+  }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Tweet $tweet)
     {
-        //
-    }
+     $tweet->delete();
+     
+    return redirect()->route('tweets.index');
+  }
 }
