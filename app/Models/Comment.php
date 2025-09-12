aab805676d66fc2bc23371d843787c4517930f8c
ruirui23 <ruirui23@users.noreply.github.com>
2025-09-12 20:26:52 +0900

    feat:Comment 機能の実装（ファイルの準備と設定）

---
+++ app/Models/Comment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /** @use HasFactory<\Database\Factories\CommentFactory> */
+    use HasFactory;
+     protected $fillable = ['comment', 'tweet_id', 'user_id'];
+
+  // 🔽 多対1の関係
+  public function tweet()
+  {
+    return $this->belongsTo(Tweet::class);
+}
+}
